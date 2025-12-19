da3d654e156e6f2ca7fc9a0e3a1fa048b9b21599
jabhiro <rajabmkr1@gmail.com>
2025-12-19 20:17:30 +0700

    Rajab_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,18 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st 
+ 
+st.write(
+    """
+    # My first app
+    Hello, Ini Project Streamlit Saya [Rajab 4232411003]!
+    """
+)
+import streamlit as st
+import pandas as pd
+ 
+st.write(pd.DataFrame({
+    'c1': [1, 2, 3, 4],
+    'c2': [10, 20, 30, 40],
+})) 
